96ddad231f83a9b91706fceb2565428d8491c814
cm <xxmail@xmailx.com>
2016-11-10 13:06:31 +0900

    初期設定

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=my_app',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => '2v-mariadoko-yXz',
 		),
 	),
 );
