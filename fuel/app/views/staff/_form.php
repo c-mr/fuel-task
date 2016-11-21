9ceb6b324ccc0aeb1a8390f893a1e79500b574ea
cm <xxmail@xmailx.com>
2016-11-21 12:10:51 +0900

    デザイン崩れを修正

--- fuel/app/views/staff/_form.php
+++ fuel/app/views/staff/_form.php
@@ -3,20 +3,22 @@
         <label class="control-label" for="form_staff_no">Staff No</label>
     </div>
     <div class="col-sm-10">
-        <input class="form-control" placeholder="Staff No" name="staff_no" value="<?= Input::post('staff_no', isset($staff) ? $staff['staff_no'] : '') ?>" type="text" id="form_staff_no" /> </div>
+        <input class="form-control" placeholder="Staff No" name="staff_no" value="<?= Input::post('staff_no', isset($staff) ? $staff['staff_no'] : '') ?>" type="text" id="form_staff_no" />
         <?php if($val->error('staff_no')):?>
-            <p class="alert alert-warning"><?php echo $val->error('staff_no'); ?></p>
+            <p class="alert alert-warning"><?= $val->error('staff_no'); ?></p>
         <?php endif; ?>
+    </div>
 </div>
 <div class="form-group">
     <div class="col-sm-2">
         <label class="control-label" for="form_name">Name</label>
     </div>
     <div class="col-sm-10">
-        <input class="form-control" placeholder="Name" name="name" value="<?= Input::post('name', isset($staff) ? $staff['name'] : '') ?>" type="text" id="form_name" /> </div>
+        <input class="form-control" placeholder="Name" name="name" value="<?= Input::post('name', isset($staff) ? $staff['name'] : '') ?>" type="text" id="form_name" />
         <?php if($val->error('name')):?>
-            <p class="alert alert-warning"><?php echo $val->error('name'); ?></p>
+            <p class="alert alert-warning"><?= $val->error('name'); ?></p>
         <?php endif; ?>
+    </div>
 </div>
 <div class="form-group">
     <div class="col-sm-2">
@@ -30,7 +32,7 @@
             <?php endforeach; ?>
         </select>
         <?php if($val->error('department')):?>
-            <p class="alert alert-warning"><?php echo $val->error('department'); ?></p>
+            <p class="alert alert-warning"><?= $val->error('department'); ?></p>
         <?php endif; ?>
     </div>
 </div>
@@ -45,7 +47,7 @@
         </label>
         <?php endforeach; ?>
         <?php if($val->error('gender')):?>
-            <p class="alert alert-warning"><?php echo $val->error('gender'); ?></p>
+            <p class="alert alert-warning"><?= $val->error('gender'); ?></p>
         <?php endif; ?>
     </div>
 </div>
