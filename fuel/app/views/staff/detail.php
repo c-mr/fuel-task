3018f7e6a9384b9cd39174b18c35f4b533f5ccf6
cm <xxmail@xmailx.com>
2016-11-21 17:19:56 +0900

    詳細画面をテーブルに変更

--- fuel/app/views/staff/detail.php
+++ fuel/app/views/staff/detail.php
@@ -4,29 +4,37 @@
     </div>
     <div class="panel-body">
 
-        <div class="row">
-            <div class="col-sm-2"><label class="control-label" for="form_staff_no">Staff No</label></div>
-            <div class="col-sm-10"><?= sprintf('%07d', $staff['staff_no']) ?></div>
-        </div>
+        <table class="table table-bordered">
+            <tbody>
+
+                <tr>
+                    <th class="active">Staff No</th>
+                    <td><?= sprintf('%07d', $staff['staff_no']) ?></td>
+                </tr>
+
+                <tr>
+                    <th class="active">Name</th>
+                    <td><?= $staff['name'] ?></td>
+                </tr>
+
+                <tr>
+                    <th class="active">Department</th>
+                    <td><?= $department_arr[$staff['department']] ?></td>
+                </tr>
+
+                <tr>
+                    <th class="active">Gender</th>
+                    <td><?= $gender_arr[$staff['gender']] ?></td>
+                </tr>
+
+            </tbody>
+        </table>
 
         <div class="row">
-            <div class="col-sm-2"><label class="control-label" for="form_name">Name</label></div>
-            <div class="col-sm-10"><?= $staff['name'] ?></div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-2"><label class="control-label" for="form_department">Department</label></div>
-            <div class="col-sm-10"><?= $department_arr[$staff['department']] ?></div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-2"><label class="control-label" for="form_gender">Gender</label></div>
-            <div class="col-sm-10"><?= $gender_arr[$staff['gender']] ?></div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-12">
-                <?php echo Html::anchor('staff/edit/'.$staff['id'],'Edit', ['class' => 'btn btn-primary']);?>
+            <div class="col-sm-1">
+                <?= Html::anchor('staff/edit/'.$staff['id'], 'Edit', ['class' => 'btn btn-primary']);?>
+            </div>
+            <div class="col-sm-1">
             <form class="form-horizontal" action="<?= Uri::create('staff/destory') ?>" name="delete_form" accept-charset="utf-8" method="post">
                 <input name="fuel_csrf_token" value="<?= Security::fetch_token() ?>" type="hidden" id="form_fuel_csrf_token" />
                 <input name="id" value="<?= $staff['id'] ?>" type="hidden" id="id" />
@@ -36,4 +44,7 @@
         </div>
 
     </div>
-</div><?php echo Asset::js('del_submit.js'); ?>
+</div>
+
+
+<?php echo Asset::js('del_submit.js'); ?>
